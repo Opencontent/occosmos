df21b4354e6c2dcafb1866fbf4ab734066c6eb8a
Luca Realdi <luca.realdi@opencontent.it>
2015-02-05 11:23:25 +0100

    Add composer.json Remove siteaccess settings

--- settings/siteaccess/cavalese_frontend/cosmos.ini.append.php
+++
@@ -1,9 +0,0 @@
-<?php #?ini charset="utf-8"?
-
-[Settings]
-Ambiti=Gli ambiti gestiti dal sistema sono: Comuni, Meteo, Trasporto extraurbano e Farmacie.
-# Cavalese
-GeonameId=6540228
-Url=http://www.cosmosinrete.it/cosmos_ws/COSMOS_WS.asmx/Cosmos_SMS_in_Geo_Place
-
-?>
--- settings/siteaccess/comunetest_frontend/cosmos.ini.append.php
+++
@@ -1,9 +0,0 @@
-<?php #?ini charset="utf-8"?
-
-[Settings]
-Ambiti=Gli ambiti gestiti dal sistema sono: Comuni, Meteo, Trasporto extraurbano e Farmacie.
-# Trento
-GeonameId=6541469
-Url=http://www.cosmosinrete.it/cosmos_ws/COSMOS_WS.asmx/Cosmos_SMS_in_Geo_Place
-
-?>
